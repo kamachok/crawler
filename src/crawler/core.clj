ff47ee440f6c9ba5a41c95bfbda8ccbf62737503
kamachok <igor.natsevich@gmail.com>
2014-12-14 23:53:32 +0300

    processing pages and find links

--- src/crawler/core.clj
+++ src/crawler/core.clj
@@ -1,7 +1,13 @@
 (ns crawler.core
+  (:import (org.apache.http.conn ConnectTimeoutException)
+           (java.net UnknownHostException))
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojurewerkz.urly.core :as urly]))
+            [clojurewerkz.urly.core :as urly]
+            [clj-http.client :as client]
+            [net.cgrand.enlive-html :as html]))
+
+(def processed (atom #{}))
 
 (defn make-tree
   ([urls depth]
@@ -11,7 +17,7 @@
 
 (defn get-indent [count]
   (->> "   "
-       (repeat )
+       (repeat)
        (take count)
        (str/join)))
 
@@ -25,8 +31,77 @@
       (println indent url)
       (dorun (map (partial print-tree (inc level)) children)))))
 
-(defn process-url [url depth]
-  {:urls [(str url depth) (str url depth)] :info {}})
+(defn is-good-status? [status]
+  (contains? #{200 201 202 203 204 205 206 207 300} status))
+
+(defn is-redirect-status? [status]
+  (contains? #{301 302 303 307} status))
+
+(defn is-already-visited? [url]
+  (contains? @(processed) url))
+
+(defn is-valid-absolute-url? [line]
+  (urly/absolute? line))
+
+(defn is-host-changed? [root-url url]
+  (let [host (.getHost (urly/url-like url))
+        parent-host (.getHost (urly/url-like root-url))]
+    (and (is-valid-absolute-url? parent-host) (not= host parent-host))))
+
+(defn is-norm-link-href? [href]
+  (nil? (re-find (re-matcher #"^((javascript|mailto|callto):|#)" href))))
+
+(defn- normalize-urls [base-url href]
+  (let [uri-href (urly/url-like href)]
+    (.toString (if (not (urly/absolute? uri-href))
+                 (urly/resolve (urly/url-like base-url) uri-href)
+                 uri-href))))
+
+(defn get-links-from-content [base-url content]
+  (let [snippet (html/html-snippet content)
+        elements (html/select snippet #{[:a]})]
+    (reduce (fn [memo link]
+              (let [href (:href (:attrs link))]
+                (if (is-norm-link-href? href)
+                  (conj memo (normalize-urls base-url href)))))
+            [] elements)))
+
+(defn http-get [url]
+  (client/get url {:throw-exceptions false
+                   :conn-timeout     2000
+                   :follow-redirects false}))
+
+(defn get-raw-responce [url]
+  (try (http-get (url))
+       (catch ConnectTimeoutException e {:status 408})
+       (catch UnknownHostException e {:status 404})
+       (catch Exception e {:status 500})))
+
+(defn wrap-info [to info]
+  (assoc to :info info))
+
+(defn get-page-info [root-url url]
+  (cond
+    (is-host-changed? root-url url) (wrap-info {:label "changed host"} {})
+    (is-already-visited? url) (wrap-info {:label "already visided"} {})
+    :else (do (swap! processed conj url)
+              (let [raw (get-raw-responce url)
+                    status (:status raw)
+                    headers (:headers raw)]
+                (cond
+                  (is-good-status? status) (wrap-info {:label "ok"} {:content raw})
+                  (is-redirect-status? status) (wrap-info {:label "redirect"} {:location (:location headers)})
+                  :else (wrap-info {:label "bad link"} {:status status}))))))
+
+(defn process-url [root-url url]
+  (let [data (get-page-info root-url url)
+        info (:info data)
+        status (:status data)]
+    (if (= status "ok")
+      (let [content (:content info)
+            urls (get-links-from-content root-url content)]
+        (assoc data :urls urls))
+      (assoc data :urls []))))
 
 (defn add-child [root child]
   (swap! (:children root) conj child))
@@ -37,18 +112,16 @@
       (travel-across-urls root)))
   ([root]
     (let [depth (:depth root)
+          root-url (:depth root)
           urls (:urls root)]
       (if (<= depth 0)
         (pmap (fn [url]
-                (let [result (process-url url depth)
+                (let [result (process-url root-url url)
                       urls (:urls result)
                       info (:info result)
                       child (make-tree url root urls info (inc depth))]
                   (add-child root child)
                   (travel-across-urls child))) urls)) root)))
-
-(defn is-valid-absolute-url? [line]
-  (urly/absolute? line))
 
 (defn read-urls-from-file [file-path]
   (-> (slurp file-path)
