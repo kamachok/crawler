a9eb983d8b4a86f1731ebb04471f62f3749b0fed
kamachok <igor.natsevich@gmail.com>
2014-12-14 22:43:35 +0300

    example + tree creating and printing

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.0.1"]
+                 [enlive "1.1.5"]
+                 [clojurewerkz/urly "1.0.0"]]
   :main crawler.core)
--- src/crawler/core.clj
+++ src/crawler/core.clj
@@ -1,20 +1,63 @@
 (ns crawler.core
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojurewerkz.urly.core :as urly]))
 
-(defn read-file-to-points [file-path]
+(defn make-tree
+  ([urls depth]
+    (assoc (make-tree "process urls:" nil urls {} (- depth)) :type "root"))
+  ([url root urls info depth]
+    {:type "child" :root root :url url :urls urls :children (atom []) :info info :depth depth}))
+
+(defn get-indent [count]
+  (->> "   "
+       (repeat )
+       (take count)
+       (str/join)))
+
+(defn print-tree
+  ([root]
+    (print-tree 0 root))
+  ([level root]
+    (let [indent (get-indent level)
+          url (:url root)
+          children @(:children root)]
+      (println indent url)
+      (dorun (map (partial print-tree (inc level)) children)))))
+
+(defn process-url [url depth]
+  {:urls [(str url depth) (str url depth)] :info {}})
+
+(defn add-child [root child]
+  (swap! (:children root) conj child))
+
+(defn travel-across-urls
+  ([urls depth]
+    (let [root (make-tree urls depth)]
+      (travel-across-urls root)))
+  ([root]
+    (let [depth (:depth root)
+          urls (:urls root)]
+      (if (<= depth 0)
+        (pmap (fn [url]
+                (let [result (process-url url depth)
+                      urls (:urls result)
+                      info (:info result)
+                      child (make-tree url root urls info (inc depth))]
+                  (add-child root child)
+                  (travel-across-urls child))) urls)) root)))
+
+(defn is-valid-absolute-url? [line]
+  (urly/absolute? line))
+
+(defn read-urls-from-file [file-path]
   (-> (slurp file-path)
       (str/split #"\n")
-      (->> (reduce
-             (fn [memo line]s
-               (conj memo (->> (str/split line #",")
-                               (butlast)
-                               (map #(Double/parseDouble (str/trim %1)))
-                               (hash-map :values)))) []))))
+      (->> (reduce #(let [url (str/trim %2)] (if (is-valid-absolute-url? url) (conj %1 url) %1)) []))))
 
 (defn parse-int [value]
   (try (if (string? value) (-> value Integer/parseInt)
-                           (value))
+                           value)
        (catch NumberFormatException e nil)))
 
 (defn is-file-exist [file-path]
@@ -32,6 +75,9 @@
     (check-and-print (>= depth 0) "Depth must be >= 0")))
 
 (defn -main [file-path depth]
-  (let [depth (parse-int depth)]
+  (let [file-path "./samples/links.txt"
+        depth (parse-int depth)]
     (if (is-valid-arguments file-path depth)
-      (println "Hello crowler"))))
+      (try
+        (print-tree (travel-across-urls (read-urls-from-file file-path) depth))
+        (finally (shutdown-agents))))))
