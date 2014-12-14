2b2268d8d62e966b2f02d0b3ae72328668d8c2d6
kamachok <igor.natsevich@gmail.com>
2014-12-15 02:18:33 +0300

    uuueeeee! last test

--- test/crawler/core_test.clj
+++ test/crawler/core_test.clj
@@ -1,8 +1,12 @@
 (ns crawler.core-test
+  (:import (java.net UnknownHostException)
+           (org.apache.http.conn ConnectTimeoutException))
   (:require [clojure.test :refer :all]
             [crawler.core :refer :all :as core]))
 
-(def test-url "http://crawler.com")
+(def test-url "http://crawler.com/")
+(def test-url-404 (str test-url 404))
+(def test-url-500 (str test-url 500))
 
 (deftest test-get-links-from-content
   (testing "Test find absolute links and relative links"
@@ -14,5 +18,27 @@
     (let [html (str "<div><a href=\"http://crawler.com/somepath\"></a><a href=\"#part\"></a><a href=\"#another/test\"></a>"
                     "<a href=\"javascript: alert(\"asdasdsa\")\"></a><a href=\"mailto:test@crawler.com\"></a><a href=\"callto:test@crawler.com\"></a></div>")
           links (core/get-links-from-content test-url html)]
-      (println links)
       (is (= (count links) 1)))))
+
+
+(defn- get-request-wrap
+  [status]
+  (case status
+    test-url-404 (throw (UnknownHostException. "404"))
+    test-url-500 (throw (Exception. "500"))))
+
+(defn wrap-setup
+  [test-fn]
+  (with-redefs-fn {#'core/http-get get-request-wrap} test-fn))
+
+(use-fixtures :each wrap-setup)
+
+(deftest handle-errors
+  (testing "Check 404 eror handling"
+    (let [raw-restonse (#'core/get-page-info test-url test-url-404)
+          label (:label raw-restonse)]
+      (is (= label "bad link"))))
+  (testing "Check 500 eror handling"
+    (let [raw-restonse (#'core/get-page-info test-url test-url-500)
+          label (:label raw-restonse)]
+      (is (= label "bad link")))))
